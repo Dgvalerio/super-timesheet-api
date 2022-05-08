93c616fbe05f066ffc8835f499783764f809d01e
Davi <al.davigvalerio@gmail.com>
2022-05-08 14:19:42 -0300

    feat: add graphql and validators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 
+import { join } from 'path';
+
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
