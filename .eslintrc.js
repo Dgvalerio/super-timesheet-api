45bc82467ef1b45e8b320bdc286f0da47ac0ce49
Davi <al.davigvalerio@gmail.com>
2022-05-07 22:58:36 -0300

    refactor: add lint rules and format code

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['@typescript-eslint/eslint-plugin', 'import-helpers'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -17,9 +17,84 @@ module.exports = {
   },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
-    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'no-console': 'warn',
+    '@typescript-eslint/consistent-type-definitions': [
+      'warn',
+      'interface'
+    ],
+    'padding-line-between-statements': [
+      'warn',
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'return'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': 'throw'
+      },
+      {
+        'blankLine': 'always',
+        'prev': [
+          'const',
+          'let',
+          'var'
+        ],
+        'next': '*'
+      },
+      {
+        'blankLine': 'always',
+        'prev': '*',
+        'next': [
+          'const',
+          'let',
+          'var'
+        ]
+      },
+      {
+        'blankLine': 'any',
+        'prev': [
+          'const',
+          'let',
+          'var'
+        ],
+        'next': [
+          'const',
+          'let',
+          'var'
+        ]
+      }
+    ],
+    'lines-between-class-members': ['error', 'always'],
+    "import-helpers/order-imports": [
+      "warn",
+      {
+        "newlinesBetween": "always",
+        "groups": [
+          "/^@nestjs/",
+          "/@/",
+          "module",
+          [
+            "parent",
+            "sibling",
+            "index"
+          ]
+        ],
+        "alphabetize": {
+          "order": "asc",
+          "ignoreCase": true
+        }
+      }
+    ],
+    "no-restricted-imports": [
+      "error",
+      {
+        "patterns": [
+          ".*"
+        ]
+      }
+    ],
   },
 };
