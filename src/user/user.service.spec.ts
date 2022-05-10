f41ad94e47b321cccd8b71872bafded087f117a3
Davi <al.davigvalerio@gmail.com>
2022-05-10 12:32:09 -0300

    test(UserService): ensure createUser throws if email has already been used

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,3 +1,4 @@
+import { ConflictException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
@@ -7,27 +8,39 @@ import { UserService } from '@/user/user.service';
 
 import { Repository } from 'typeorm';
 
-const UserRepositoryStub: Partial<Repository<User>> = {
-  find: jest
-    .fn()
-    .mockReturnValue([makeFakeUser(), makeFakeUser(), makeFakeUser()]),
+const makeUserRepositoryStub = (): Partial<Repository<User>> => {
+  class UserRepositoryStub implements Partial<Repository<User>> {
+    user = makeFakeUser();
+
+    find = jest
+      .fn()
+      .mockReturnValue([makeFakeUser(), makeFakeUser(), makeFakeUser()]);
+
+    findBy = jest.fn().mockReturnValue([this.user]);
+  }
+
+  return new UserRepositoryStub();
 };
 
 describe('UserService', () => {
   let userService: UserService;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
+    jest.resetAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         UserService,
         {
           provide: getRepositoryToken(User),
-          useValue: UserRepositoryStub,
+          useValue: makeUserRepositoryStub(),
         },
       ],
     }).compile();
 
     userService = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
   it('should be defined', () => {
@@ -39,7 +52,22 @@ describe('UserService', () => {
       const users = await userService.getAllUsers();
 
       expect(users).toHaveLength(3);
-      expect(UserRepositoryStub.find).toHaveBeenCalledTimes(1);
+      expect(userRepository.find).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should throws if email has already been used', async () => {
+      const fakeUser = makeFakeUser();
+
+      await expect(userService.createUser(fakeUser)).rejects.toThrow(
+        new ConflictException('Esse email já foi utilizado!'),
+      );
+
+      expect(userRepository.findBy).toHaveBeenCalledTimes(1);
+      expect(userRepository.findBy).toHaveBeenCalledWith({
+        email: fakeUser.email,
+      });
     });
   });
 });
