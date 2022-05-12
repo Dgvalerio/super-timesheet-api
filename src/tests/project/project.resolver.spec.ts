de5c16470f6ca73ec2ed7e2d49188659955cebea
Davi <al.davigvalerio@gmail.com>
2022-05-12 19:29:29 -0300

    test: add initial tests to project

---
+++ src/tests/project/project.resolver.spec.ts
@@ -0,0 +1,19 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { ProjectResolver } from '@/project/project.resolver';
+
+describe('ProjectResolver', () => {
+  let resolver: ProjectResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [ProjectResolver],
+    }).compile();
+
+    resolver = module.get<ProjectResolver>(ProjectResolver);
+  });
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+});
