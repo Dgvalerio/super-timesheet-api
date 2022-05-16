1ed860ab77be6cb479b781f92fc78f69c615d19b
Davi <al.davigvalerio@gmail.com>
2022-05-15 22:17:21 -0300

    test(delete-user): [e2e] ensure delete user correctly

--- test/user/user.e2e-spec.ts
+++ test/user/user.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { AuthOutput } from '@/auth/dto/auth.output';
 import { CreateUserInput } from '@/user/dto/create-user.input';
+import { DeleteUserInput } from '@/user/dto/delete-user.input';
 import { GetUserInput } from '@/user/dto/get-user.input';
 import { User } from '@/user/user.entity';
 import { randWord } from '@ngneat/falso';
@@ -11,6 +12,7 @@ import {
 } from '!/collaborators/apolloClient';
 import { makeCreateUserInput } from '!/user/collaborators/makeCreateUserInput';
 import { makeCreateUserMutation } from '!/user/collaborators/makeCreateUserMutation';
+import { makeDeleteUserMutation } from '!/user/collaborators/makeDeleteUserMutation';
 import { makeGetAllUsersQuery } from '!/user/collaborators/makeGetAllUsersQuery';
 import { makeGetUserQuery } from '!/user/collaborators/makeGetUserQuery';
 
@@ -288,6 +290,106 @@ describe('Graphql User Module (e2e)', () => {
       const { graphQLErrors } = await out.catch((e) => e);
 
       expect(graphQLErrors[0].message).toBe('Nenhum usuário foi encontrado');
+      expect(graphQLErrors[0].extensions).toHaveProperty('response');
+
+      const { response } = graphQLErrors[0].extensions;
+
+      expect(response.statusCode).toBe(404);
+      expect(response.error).toBe('Not Found');
+    });
+  });
+
+  describe('deleteUser', () => {
+    let user: User;
+    let client: ApolloClient<NormalizedCacheObject>;
+
+    const makeOut = async (input: Partial<DeleteUserInput>) =>
+      client.mutate<{ deleteUser: boolean }>({
+        mutation: makeDeleteUserMutation(input),
+      });
+
+    beforeAll(async () => {
+      const createUserInput = makeCreateUserInput();
+
+      await apolloClient.mutate<{ createUser: User }>({
+        mutation: makeCreateUserMutation(createUserInput),
+      });
+
+      const {
+        data: {
+          login: { token },
+        },
+      } = await apolloClient.mutate<{ login: AuthOutput }>({
+        mutation: makeLoginMutation({
+          email: createUserInput.email,
+          password: createUserInput.password,
+        }),
+      });
+
+      client = apolloAuthorizedClient(token);
+    });
+
+    beforeEach(async () => {
+      const createUserInput = makeCreateUserInput();
+
+      const { data } = await apolloClient.mutate<{ createUser: User }>({
+        mutation: makeCreateUserMutation(createUserInput),
+      });
+
+      user = data.createUser;
+    });
+
+    it('should throw if unauthenticated', async () => {
+      const out = apolloClient.mutate<{ deleteUser: boolean }>({
+        mutation: makeDeleteUserMutation({}),
+      });
+
+      const { graphQLErrors } = await out.catch((e) => e);
+
+      expect(graphQLErrors[0].message).toBe('Unauthorized');
+      expect(graphQLErrors[0].extensions).toHaveProperty('response');
+      expect(graphQLErrors[0].extensions.response.statusCode).toBe(401);
+    });
+
+    it('should throw if no parameter as entered', async () => {
+      const out = makeOut({});
+
+      const { graphQLErrors } = await out.catch((e) => e);
+
+      expect(graphQLErrors[0].message).toBe('Bad Request Exception');
+      expect(graphQLErrors[0].extensions).toHaveProperty('response');
+
+      const { response } = graphQLErrors[0].extensions;
+
+      expect(response.message[0]).toBe('id should not be empty');
+      expect(response.message[1]).toBe('id must be a string');
+      expect(response.message[2]).toBe('email should not be empty');
+      expect(response.message[3]).toBe('email must be an email');
+
+      expect(response.statusCode).toBe(400);
+      expect(response.error).toBe('Bad Request');
+    });
+
+    it('should delete user by id', async () => {
+      const { data } = await makeOut({ id: user.id });
+
+      expect(data).toHaveProperty('deleteUser');
+      expect(data).toBeTruthy();
+    });
+
+    it('should delete user by email', async () => {
+      const { data } = await makeOut({ email: user.email });
+
+      expect(data).toHaveProperty('deleteUser');
+      expect(data).toBeTruthy();
+    });
+
+    it('should throw if not found user', async () => {
+      const out = makeOut({ email: `${randWord()}_${user.email}` });
+
+      const { graphQLErrors } = await out.catch((e) => e);
+
+      expect(graphQLErrors[0].message).toBe('O usuário informado não existe!');
       expect(graphQLErrors[0].extensions).toHaveProperty('response');
 
       const { response } = graphQLErrors[0].extensions;
