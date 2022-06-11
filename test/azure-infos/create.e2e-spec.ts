5a312e1720c251dd02679f01f70b94f439230037
Davi <al.davigvalerio@gmail.com>
2022-06-11 18:17:44 -0300

    test(create-azure-infos): should throw if enter a empty login

--- test/azure-infos/create.e2e-spec.ts
+++ test/azure-infos/create.e2e-spec.ts
@@ -1,11 +1,35 @@
+import { AzureInfos } from '@/azure-infos/azure-infos.entity';
+import { CreateAzureInfosInput } from '@/azure-infos/dto/create-azure-infos.input';
+
+import { makeCreateAzureInfosInput } from '!/azure-infos/collaborators/makeCreateAzureInfosInput';
 import { makeCreateAzureInfosMutation } from '!/azure-infos/collaborators/makeCreateAzureInfosMutation';
 import { ApolloClientHelper } from '!/collaborators/apolloClient';
-import { shouldThrowIfUnauthenticated } from '!/collaborators/helpers';
+import {
+  shouldThrowIfEnterAEmptyParam,
+  shouldThrowIfUnauthenticated,
+} from '!/collaborators/helpers';
 
 describe('[E2E] Azure Infos > Create', () => {
   const api = new ApolloClientHelper();
 
+  const makeOut = async (input: Partial<CreateAzureInfosInput>) =>
+    api.mutation<{ createAzureInfos: AzureInfos }>(
+      makeCreateAzureInfosMutation(input),
+    );
+
   beforeAll(async () => await api.authenticate());
 
   shouldThrowIfUnauthenticated('mutation', makeCreateAzureInfosMutation({}));
+
+  it('should throw if enter a empty login', async () => {
+    const input = makeCreateAzureInfosInput();
+
+    input.login = '';
+
+    const out = makeOut(input);
+
+    const { graphQLErrors } = await out.catch((e) => e);
+
+    shouldThrowIfEnterAEmptyParam('login', graphQLErrors);
+  });
 });
