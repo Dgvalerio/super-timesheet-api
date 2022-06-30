255a2398bf1ff00eb5aa879b391275b49ee55d58
Davi <al.davigvalerio@gmail.com>
2022-06-29 23:58:21 -0300

    build(heroku): config database access

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,22 @@
-module.exports = {
+const common = {
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
   synchronize: process.env.DB_SYNC,
 };
+
+module.exports = process.env.DATABASE_URL
+  ? {
+      ...common,
+      url: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    }
+  : {
+      ...common,
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+    };
